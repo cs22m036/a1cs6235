40d49468d25e96570e92479302f6a39aa470cbc1
Ramya Kasaraneni <ramyakasaraneni@Ramyas-MacBook-Pro.local>
2023-02-18 12:23:40 +0530

    First commit

---
+++ inputs/P1.java
@@ -0,0 +1,27 @@
+
+class P1 {
+  void m1() {
+	 A x,y;
+	 x = new A();
+	 y = new A();
+	 x.foo();
+	 y.foo();
+	 
+ }
+  void m2() {
+	  A a,b,z;
+	  boolean c = true;
+		 a = new A();
+		 b = new A();
+		 if(c)
+		  z = a;
+		 else
+			z = b;
+		 z.foo();
+ }
+}
+class A{
+	void foo() {
+		System.out.println(10);
+	}
+}
---
+++ src/submit_a1/AliasAnalysis.java
@@ -0,0 +1,19 @@
+package submit_a1;
+
+import java.util.Map;
+
+import soot.Body;
+import soot.BodyTransformer;
+
+public class AliasAnalysis extends BodyTransformer{
+
+	@Override
+	protected void internalTransform(Body arg0, String arg1, Map<String, String> arg2) {
+		/*
+		 * Implement your alias analysis here. A1.answers should include the Yes/No answers for 
+		 * the queries
+		 */
+		
+	}
+	
+}
